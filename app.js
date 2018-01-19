245aa74e9d53628e20fe4bb249c80f538e663ce9
RJ <jensenrs@gmail.com>
2018-01-19 14:56:34 -0800

    add basic packages

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
+
+app.disable('x-powered-by')
+app.use(bodyParser.json())
+app.use(morgan('dev'))
+app.use(cors())
+
+//const Router...
+
+app.use((err, req, res, next) => {
+  const status = err.status || 500
+  const message = err.message || "Sorry, something went wrong."
+  res.status(status).json({error: {message}})
+})
+
+app.use((req, res, next) => {
+  const status = 404
+  const message = `Could not ${req.method}${req.path}`
+  res.status(404).json({status, message})
+})
+
+const listener = () => console.log(`Listening on port ${port}`);
+app.listen(port, listener)
+
+module.exports = app
