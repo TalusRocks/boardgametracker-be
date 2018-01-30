7d8ae75dc1b62a5078a9d28a85c9141e976a4059
RJ <jensenrs@gmail.com>
2018-01-30 12:54:55 -0800

    added game_name to plays table

--- db/seeds/03_plays.js
+++ db/seeds/03_plays.js
@@ -5,11 +5,11 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('plays').insert([
-        {id: 1, user_id: 1, bgg_game_id: 194607, comment: 'close game!', played_on: '2018-01-05'},
-        {id: 2, user_id: 1, bgg_game_id: 242301, comment: 'fun', played_on: '2018-01-06'},
-        {id: 3, user_id: 1, bgg_game_id: 219102, comment: 'wow', played_on: '2018-01-07'},
-        {id: 4, user_id: 1, bgg_game_id: 214329, comment: 'rad', played_on: '2018-01-07'},
-        {id: 5, user_id: 1, bgg_game_id: 229980, comment: 'yeah', played_on: '2018-01-08'}
+        {id: 1, user_id: 1, game_name: 'seed 1', bgg_game_id: 123, comment: 'close game!', played_on: '2018-01-05'},
+        {id: 2, user_id: 1, game_name: 'seed 2', bgg_game_id: 1234, comment: 'fun', played_on: '2018-01-06'},
+        {id: 3, user_id: 1, game_name: 'seed 3', bgg_game_id: 12345, comment: 'wow', played_on: '2018-01-07'},
+        {id: 4, user_id: 1, game_name: 'seed 4', bgg_game_id: 123456, comment: 'rad', played_on: '2018-01-07'},
+        {id: 5, user_id: 1, game_name: 'seed 5', bgg_game_id: 1234567, comment: 'yeah', played_on: '2018-01-08'}
       ])
     }).then(() => {
     return knex.raw(`SELECT setval('plays_id_seq', (SELECT MAX(id) FROM plays));`)
